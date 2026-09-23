c711c3c3288026978d3c6381cd54763c153a1b9a
Geovani P. Duqueza <geovaniduqueza1939@outlook.com>
2026-09-23 15:59:14 +0800

    Added sample layout viewer
    * Also refactored test routes

--- src/modules/controller.mjs
+++ src/modules/controller.mjs
@@ -25,85 +25,7 @@ export default class controller {
             res.render('index', { baseDir: config.baseDir, name: '' });
         });
 
-        this.#router.get(`${config.baseDir}/login`, (req, res) => {
-            res.render('login1', { baseDir: config.baseDir, username: '', password: '' });
-        });
-
-        this.#router.post(`${config.baseDir}/login`, (req, res) => {
-            res.render('login1', { baseDir: config.baseDir, username: req.body.username || '', password: req.body.password || '' });
-        });
-
-        const loginDesigns = [1, 2, 3, 4];
-        // for (const design of loginDesigns) {
-        //     this.#router.get(`${config.baseURL}/login/${design}`, (req, res) => {
-        //         res.render(`login${design}`, { baseDir: config.baseDir, username: '', password: '' });
-        //     });
-
-        //     this.#router.post(`${config.baseURL}/login/${design}`, (req, res) => {
-        //         res.render(`login${design}`, { baseDir: config.baseDir, username: req.body.username || '', password: req.body.password || '' });
-        //     });
-        // }
-
-        this.#router.get(`${config.baseDir}/login/:id`, (req, res) => {
-            if (loginDesigns.includes(parseInt(req.params.id))) {
-                res.render(`login${req.params.id}`, { baseDir: config.baseDir, username: '', password: '' });
-            }
-            else {
-                res.status(404).render('error', { baseDir: config.baseDir, message: 'Login page not found.', statusCode: 404, app_name: config.shortname });
-                return;
-            }
-        });
-
-        this.#router.post(`${config.baseDir}/login/:id`, (req, res) => {
-            if (loginDesigns.includes(parseInt(req.params.id))) {
-                res.render(`login${req.params.id}`, { baseDir: config.baseDir, username: req.body.username || '', password: req.body.password || '' });
-            }
-            else {
-                res.status(404).render('error', { baseDir: config.baseDir, message: 'Login page not found.', statusCode: 404, app_name: config.shortname });
-                return;
-            }
-        });
-
-        const sspDesigns = [1, 2, 3];
-
-        this.#router.get(`${config.baseDir}/ssp/:id`, (req, res) => {
-            if (sspDesigns.includes(parseInt(req.params.id))) {
-                res.render(`ssp${req.params.id}`, { baseDir: config.baseDir });
-            }
-            else {
-                res.status(404).render('error', { baseDir: config.baseDir, message: 'Self-Service Portal page not found.', statusCode: 404, app_name: config.shortname });
-                return;
-            }
-        });
-
-
-        const mainDesigns = [1, 2, 3];
-
-        const renderMain = (req, res) => {
-            res.render(`main1`, { baseDir: config.baseDir });
-        };
-
-        const renderMainById = (req, res) => {
-            if (mainDesigns.includes(parseInt(req.params.id))) {
-                res.render(`main${req.params.id}`, { baseDir: config.baseDir });
-            }
-            else {
-                res.status(404).render('error', { baseDir: config.baseDir, message: 'HRMO main page not found.', statusCode: 404, app_name: config.shortname });
-                return;
-            }
-        };
-
-        this.#router.get(`${config.baseDir}/main`, renderMain);
-        this.#router.get(`${config.baseDir}/main/:id`, renderMainById);
-
-        // TEMPORARY TEST ROUTES
-        const renderTest = (req, res) => {
-            res.render('test', { baseDir: config.baseDir, testdata: req });
-        }
-
-        this.#router.get(`${config.baseDir}/test`, renderTest);
-
-        this.#router.post(`${config.baseDir}/test`, renderTest);
+        this.#setupTestRoutes();
 
         this.#app.use(this.#router);
 
@@ -123,9 +45,75 @@ export default class controller {
         });
     }
 
+    // TEMPORARY TEST ROUTES : BEGIN
+    #setupTestRoutes() {
+        const loginDesigns = [1, 2, 3, 4];
+
+        const renderLogin = (req, res) => {
+            if (loginDesigns.includes(parseInt(req.params.id))) {
+                res.render(`login${req.params.id}`, { baseDir: config.baseDir, username: req.body?.username || '', password: req.body?.password || '' });
+            }
+            else {
+                res.status(404).render('error', { baseDir: config.baseDir, message: 'Login page not found.', statusCode: 404, app_name: config.shortname });
+                return;
+            }
+        }
+
+        this.#router.get(`${config.baseDir}/test/login/:id`, renderLogin);
+        this.#router.post(`${config.baseDir}/test/login/:id`, renderLogin);
+        this.#router.get(`${config.baseDir}/test/login`, this.#sampleLayoutViewer("Login", loginDesigns, `${config.baseDir}/test/login`));
+
+        const sspDesigns = [1, 2, 3];
+
+        const renderSSP = (req, res) => {
+            if (sspDesigns.includes(parseInt(req.params.id))) {
+                res.render(`ssp${req.params.id}`, { baseDir: config.baseDir });
+            }
+            else {
+                res.status(404).render('error', { baseDir: config.baseDir, message: 'Self-Service Portal page not found.', statusCode: 404, app_name: config.shortname });
+                return;
+            }
+        };
+
+        this.#router.get(`${config.baseDir}/test/ssp/:id`, renderSSP);
+        this.#router.post(`${config.baseDir}/test/ssp/:id`, renderSSP);
+        this.#router.get(`${config.baseDir}/test/ssp`, this.#sampleLayoutViewer("SSP", sspDesigns, `${config.baseDir}/test/ssp`));
+
+        const mainDesigns = [1, 2, 3];
+
+        const renderMainById = (req, res) => {
+            if (mainDesigns.includes(parseInt(req.params.id))) {
+                res.render(`main${req.params.id}`, { baseDir: config.baseDir });
+            }
+            else {
+                res.status(404).render('error', { baseDir: config.baseDir, message: 'HRMO main page not found.', statusCode: 404, app_name: config.shortname });
+                return;
+            }
+        };
+
+        this.#router.get(`${config.baseDir}/test/main/:id`, renderMainById);
+        this.#router.post(`${config.baseDir}/test/main/:id`, renderMainById);
+        this.#router.get(`${config.baseDir}/test/main`, this.#sampleLayoutViewer("Main", mainDesigns, `${config.baseDir}/test/main`));
+
+        const renderTest = (req, res) => {
+            res.render('test', { baseDir: config.baseDir, testdata: req });
+        }
+        this.#router.get(`${config.baseDir}/test`, renderTest);
+        this.#router.post(`${config.baseDir}/test`, renderTest);
+    }
+    // TEMPORARY TEST ROUTES : END
+
+
+
     #showErrorPage(err, friendlyErrorMessage, consoleErrorMessage, res, statusCode = 0) {
         statusCode = (statusCode === 0 ? ((err && err.statusCode) || 500) : statusCode);
         this.#logger.error(consoleErrorMessage);
         res.status(statusCode).render('error', { baseDir: config.baseDir, message: friendlyErrorMessage, statusCode, app_name: config.shortname });
+    }
+
+    #sampleLayoutViewer(sampleLayoutName, sampleLayoutIds, url) {
+        return (req, res) => {
+            res.render('testsample', { sampleLayoutName, sampleLayoutIds, url });
+        };
     }
 }
